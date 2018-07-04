0ad4005bad70179bbae87e8834d8dad64735d3b2
HannibalKcc <zhuhuaruicn@163.com>
2018-07-04 17:21:40 +0800

    可以递归遍历赞同列表了

--- src/getApprovers.js
+++ src/getApprovers.js
@@ -7,25 +7,53 @@ async function getApprovers ({page, tar}) {
   }
   
   await page.goto(tar);
-  // TODO 滚动获取更多
   let nodeBtn = await page.$('.Card .Button--plain');
   await nodeBtn.click(nodeBtn);
+  let approversCounts = await getApproversCounts(page); // 获取点赞者总数
   await page.waitForSelector('.VoterList-content .List-item .UserLink-link');
-  await page.evaluate(() => {
-    document.querySelector('.VoterList-content').scrollTop = 999;
-    // TODO 等待渲染 继续滚动
-  });
-  // nodeListBox.scrollTop = 999; // 直接设置滚动到底
-  // let nodeLinkList = await page.$$eval('.VoterList-content .List-item .UserLink-link', node => node);
-  // let linkList = nodeLinkList.map(node => node.href) // 获取点赞者个人页面
+  await page.evaluate(...scrollToCounts(page, approversCounts));
   
   return;
-  
-  let data = [{date: 1, data: 2}] // TODO
-  linkList.forEach(async link => {
-    // await getActivies(page, link)
-  })
-  addVotesLineImg({data}); // 数据生成图片
+  // TODO addVotesLineImg({data}); // 数据生成图片
+}
+
+/**
+ * 通过检索所有的ajax res，找出点赞者总数
+ * */
+async function getApproversCounts (page) {
+  return new Promise((resolve, reject) => {
+    page.on('response', responseEvent);
+    
+    async function responseEvent (res) {
+      if ((/https:\/\/www.zhihu.com\/api\/v4\/answers\/\d+\/voters.*/).test(res.url())) {
+        page.removeListener('response', responseEvent);
+        resolve((await res.json()).paging.totals);
+      }
+    }
+  });
+}
+
+/**
+ * 控制无限滚动条 迭代滚动到指定位置
+ * @return <Function> 控制滚动的函数
+ * */
+function scrollToCounts (page, maxApproversCounts = 0, limit = 100) {
+  return [
+    async (maxApproversCounts, limit) => {
+      console.log(document.body);
+      let nodeList = document.querySelector('.VoterList-content');
+      let max = limit > 0 ? limit : maxApproversCounts;
+      for (let curNum = 0; curNum < max; curNum += 10) {
+        nodeList.scrollTop = nodeList.scrollHeight;
+        await new Promise((resolve, reject)=>{
+          setTimeout(()=>{
+            resolve()
+          },1e3); // 改进：除了setTimeout有没有更好的滚动方法了呢？
+        });
+      }
+    },
+    maxApproversCounts, limit
+  ];
 }
 
 module.exports = getApprovers
